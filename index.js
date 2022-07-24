66b1bdf9a2f5e0f88a5c702ffb7138353a70efc6
= <josephajibodu@gmail.com>
2022-07-24 10:11:13 +0100

    I resolved a little bug by placing it in if statement to check that its x axis.
    You know this conditional check is for x

--- index.js
+++ index.js
@@ -42,18 +42,20 @@ function move(axis, distance) {
   // Lets check for x boundary first
   // Note: x,y refers to the top left corner.
   // With a box size of 120
-  // If the window width is 420, then it means our box will be touching the edge already when its at x=300 (Think about it) 
-  // If the window width is 420, also our box will be touching the edge already when its at x=0 (Think about it) 
+  // If the window width is 420, then it means our box will be touching the edge already when its at x=300 (Think about it)
+  // If the window width is 420, also our box will be touching the edge already when its at x=0 (Think about it)
   // So our boundary for the width are x=0 and x=300 (420 - 120)
   // SO lets code it
-  let newX = x + distance; //Trying to compute the new x without modifying the normal x yet
-  // You can combine the checks, but let me separate it for your understanding
-  // Check 1: x=0 (we don't want x to be less that 0)
-  if (newX < 0) return; 
-  // Check 2: x=300 (420 - 120) (we don't want x to be greater that 300(according to our assumptions))
-  if (newX > (WINDOW_WIDTH - BOX_WIDTH)) return; 
+  if (axis === "x") {
+    let newX = x + distance; //Trying to compute the new x without modifying the normal x yet
+    // You can combine the checks, but let me separate it for your understanding
+    // Check 1: x=0 (we don't want x to be less that 0)
+    if (newX < 0) return;
+    // Check 2: x=300 (420 - 120) (we don't want x to be greater that 300(according to our assumptions))
+    if (newX > WINDOW_WIDTH - BOX_WIDTH) return;
+  }
 
-  if (axis === 'x') {
+  if (axis === "x") {
     x += distance;
     myBox.style.left = x + "px";
   } else {
